8cc8400ff900ed6485983d0992560f2a64c15041
Ivan Nečas <necasik@gmail.com>
2019-06-02 08:11:03 +0200

    Convert find or create function to method

--- src/FSharpSqlite.Use/Program.fs
+++ src/FSharpSqlite.Use/Program.fs
@@ -13,37 +13,39 @@ module internal Crypto =
     
 type Sql = FsharpSqlite.Use.SqlProvider.Sql
 
-let findOrCreateUser
-    (dc : Sql.dataContext)
-    (firstName: string) (lastName: string) (email: string) (password: string) : Sql.dataContext.``main.UsersEntity`` =
-    let userQuery = query {
-        for user in dc.Main.Users do
-            where (user.Email = email)
-            take 1
-            select (user)
-    }
-    let maybeUser = userQuery |> Seq.tryHead
-    match maybeUser with
-    | None ->
-        let user =  dc.Main.Users.Create()
-        user.FirstName <- firstName
-        user.LastName <- lastName
-        user.Email <- email
-        let rngProvider = new Crypto.RNGCryptoServiceProvider()
+type Persistence() =
+    let dc = Sql.GetDataContext()
+        
+    member __.findOrCreateUser
+        (firstName: string) (lastName: string) (email: string) (password: string) : Sql.dataContext.``main.UsersEntity`` =
+        let userQuery = query {
+            for user in dc.Main.Users do
+                where (user.Email = email)
+                take 1
+                select (user)
+        }
+        let maybeUser = userQuery |> Seq.tryHead
+        match maybeUser with
+        | None ->
+            let user =  dc.Main.Users.Create()
+            user.FirstName <- firstName
+            user.LastName <- lastName
+            user.Email <- email
+            let rngProvider = new Crypto.RNGCryptoServiceProvider()
 
-        let saltBytes = Array.create 10 (new byte())
-        rngProvider.GetBytes(saltBytes)
-        user.PasswordSalt <- Convert.ToBase64String(saltBytes)
-        let passwordSaltBytes = Array.append <| Text.Encoding.UTF8.GetBytes(password) <| saltBytes
-        user.PasswordHash <- Convert.ToBase64String(Crypto.SHA256.Create().ComputeHash(passwordSaltBytes))
-        dc.SubmitUpdates()
-        user
-    | Some user -> user
+            let saltBytes = Array.create 10 (new byte())
+            rngProvider.GetBytes(saltBytes)
+            user.PasswordSalt <- Convert.ToBase64String(saltBytes)
+            let passwordSaltBytes = Array.append <| Text.Encoding.UTF8.GetBytes(password) <| saltBytes
+            user.PasswordHash <- Convert.ToBase64String(Crypto.SHA256.Create().ComputeHash(passwordSaltBytes))
+            dc.SubmitUpdates()
+            user
+        | Some user -> user
 
 
 let WorkWithDb =
-    let dc = Sql.GetDataContext()
-    let john = findOrCreateUser dc "John" "Lenon" "john@beatles.com" "HeyJude123"
+    let p = new Persistence()
+    let john = p.findOrCreateUser "John" "Lenon" "john@beatles.com" "HeyJude123"
     printfn "John's id is %d" john.Id
     ()
 
