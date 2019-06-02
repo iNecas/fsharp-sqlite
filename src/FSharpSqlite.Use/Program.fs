7135580f37466f4b220c5cf4155836799ea91fde
Ivan Nečas <necasik@gmail.com>
2019-06-02 09:56:22 +0200

    Refactor

--- src/FSharpSqlite.Use/Program.fs
+++ src/FSharpSqlite.Use/Program.fs
@@ -35,8 +35,7 @@ type PasswordHash with
         let passwordSaltBytes = Array.append <| Text.Encoding.UTF8.GetBytes(password) <| saltBytes
         let hash = Convert.ToBase64String(Crypto.SHA256.Create().ComputeHash(passwordSaltBytes))
         { Hash = hash; Salt = salt }
-
-
+        
 type User = {
     FirstName: string
     LastName: string
@@ -44,44 +43,60 @@ type User = {
     Password: Password
 }
 
+type PersistedUser = {
+    Id: int64
+    User: User
+}
+
 type Persistence() =
     let dc = Sql.GetDataContext()
-        
-    member __.findOrCreateUser (user: User) : Sql.dataContext.``main.UsersEntity`` =
-        let userQuery = query {
+    
+    member this.CreateUser (user: User) : PersistedUser =
+        let u =  dc.Main.Users.Create()
+        u.FirstName <- user.FirstName
+        u.LastName <- user.LastName
+        u.Email <- user.Email
+        let passwordHash =
+            match user.Password with
+            | PlainTextPassword password ->  PasswordHash.OfPlainTextPassword password
+            | PasswordHash passwordHash -> passwordHash
+        u.PasswordHash <- passwordHash.Hash
+        u.PasswordSalt <- passwordHash.Salt
+            
+        dc.SubmitUpdates()
+        { Id = u.Id; User = { user with Password = PasswordHash passwordHash } }
+     
+     member this.FindUserByEmail (email: string) : PersistedUser option =
+         let userQuery = query {
             for u in dc.Main.Users do
-                where (u.Email = user.Email)
+                where (u.Email = email)
                 take 1
                 select (u)
-        }
-        let maybeUser = userQuery |> Seq.tryHead
-        match maybeUser with
-        | None ->
-            let u =  dc.Main.Users.Create()
-            u.FirstName <- user.FirstName
-            u.LastName <- user.LastName
-            u.Email <- user.Email
-            let passwordHash =
-                match user.Password with
-                | PlainTextPassword password ->  PasswordHash.OfPlainTextPassword password
-                | PasswordHash passwordHash -> passwordHash
-            u.PasswordHash <- passwordHash.Hash
-            u.PasswordSalt <- passwordHash.Salt
-                
-            dc.SubmitUpdates()
-            u
+         }
+         let recordToUser (record: Sql.dataContext.``main.UsersEntity``) =
+             let user = { FirstName = record.FirstName
+                          LastName = record.LastName
+                          Email = record.Email
+                          Password = PasswordHash { Hash = record.PasswordHash; Salt = record.PasswordSalt } }
+             { Id = record.Id; User = user }
+         userQuery |> Seq.tryHead |> Option.map recordToUser
+         
+        
+    member this.FindOrCreateUser (user: User) : PersistedUser =
+        match this.FindUserByEmail(user.Email) with
+        | None -> this.CreateUser user
         | Some u -> u
 
 
 let WorkWithDb =
     let p = new Persistence()
     let john = { FirstName = "John"; LastName = "Lenon"; Email = "john@beatles.com"; Password = PlainTextPassword("HeyJude123") }
-    let johnRecord = p.findOrCreateUser john
-    printfn "John's id is %d" johnRecord.Id
+    let persistedJohn = p.FindOrCreateUser john
+    printfn "John's id is %d" persistedJohn.Id
     ()
 
 [<EntryPoint>]
 let main argv =
     let ret = 0
     WorkWithDb
-    0 // return an integer exit code
+    ret
